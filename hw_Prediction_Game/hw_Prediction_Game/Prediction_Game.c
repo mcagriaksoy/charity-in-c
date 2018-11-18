413f9741bef73623edd17da20f5ea5d704e2763a
Organick <mcagriaksoy@ya.ru>
2018-11-18 23:34:59 +0300

    Add files via upload

---
+++ hw_Prediction_Game/hw_Prediction_Game/Prediction_Game.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdlib.h> 
+#define SIZE	10
+
+int arr[SIZE];
+int random_num;
+int i,x,truth_rate;
+ 
+int main() {
+    for(i=0; i<SIZE ;i++){
+    		
+    		random_num = rand()%10;
+            arr[i]=random_num;
+            //printf("%d\n", arr[i]);    
+            }
+            
+	truth_rate = 0;
+		
+    for(i=0; i<SIZE ;i++){
+	 	printf("Guess %dth number:",i);
+	 	scanf("%d", &x);
+	 	if(x == arr[i]){
+		 	truth_rate++;
+		 	}
+		 }
+
+	 printf("Game over\nSuccess in game result: %d %%",truth_rate*10);
+scanf("%d", &i);	 	 
+}
